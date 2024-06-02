01869ed00e0bc0ea3a1741fcd2221da98c2b3837
你猜 <117290962+GoldenStain@users.noreply.github.com>
2024-06-03 02:43:31 +0800

    创建记忆库

---
+++ Translate2/ExtendFunction/MemoryBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Translate2.ExtendFunction;
+
+namespace Translate2.MemoryBase
+{
+
+}
