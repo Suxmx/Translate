ce7b2f19d7d0ca5e60a0588e4ff254c4f1cc650e
你猜 <117290962+GoldenStain@users.noreply.github.com>
2024-06-03 19:29:01 +0800

    final update

--- Translate2/SubViews/MachineTranslate.Designer.cs
+++ Translate2/SubViews/MachineTranslate.Designer.cs
@@ -48,12 +48,11 @@
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(281, 246);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(422, 369);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -64,34 +63,31 @@
             this.tableLayoutPanel2.Controls.Add(this.inputBox, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.outputBox, 1, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(2, 2);
-            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(277, 210);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(416, 315);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
             // inputBox
             // 
             this.inputBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.inputBox.Location = new System.Drawing.Point(2, 2);
-            this.inputBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inputBox.Location = new System.Drawing.Point(3, 3);
             this.inputBox.Multiline = true;
             this.inputBox.Name = "inputBox";
-            this.inputBox.Size = new System.Drawing.Size(134, 206);
+            this.inputBox.Size = new System.Drawing.Size(202, 309);
             this.inputBox.TabIndex = 0;
             // 
             // outputBox
             // 
             this.outputBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.outputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputBox.Location = new System.Drawing.Point(140, 2);
-            this.outputBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.outputBox.Font = new System.Drawing.Font("HarmonyOS Sans SC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputBox.Location = new System.Drawing.Point(211, 3);
             this.outputBox.Multiline = true;
             this.outputBox.Name = "outputBox";
-            this.outputBox.Size = new System.Drawing.Size(135, 206);
+            this.outputBox.Size = new System.Drawing.Size(202, 309);
             this.outputBox.TabIndex = 1;
             // 
             // tableLayoutPanel3
@@ -102,12 +98,13 @@
             this.tableLayoutPanel3.Controls.Add(this.translateBtn, 0, 0);
             this.tableLayoutPanel3.Controls.Add(this.fillBtn, 1, 0);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 217);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(4, 325);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 1;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(275, 26);
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(414, 40);
             this.tableLayoutPanel3.TabIndex = 2;
             // 
             // translateBtn
@@ -118,7 +115,7 @@
             this.translateBtn.Location = new System.Drawing.Point(0, 0);
             this.translateBtn.Margin = new System.Windows.Forms.Padding(0);
             this.translateBtn.Name = "translateBtn";
-            this.translateBtn.Size = new System.Drawing.Size(137, 26);
+            this.translateBtn.Size = new System.Drawing.Size(207, 40);
             this.translateBtn.TabIndex = 0;
             this.translateBtn.Text = "翻译";
             this.translateBtn.UseVisualStyleBackColor = true;
@@ -129,10 +126,10 @@
             this.fillBtn.AutoSize = true;
             this.fillBtn.Dock = System.Windows.Forms.DockStyle.Fill;
             this.fillBtn.Font = new System.Drawing.Font("微软雅黑", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.fillBtn.Location = new System.Drawing.Point(137, 0);
+            this.fillBtn.Location = new System.Drawing.Point(207, 0);
             this.fillBtn.Margin = new System.Windows.Forms.Padding(0);
             this.fillBtn.Name = "fillBtn";
-            this.fillBtn.Size = new System.Drawing.Size(138, 26);
+            this.fillBtn.Size = new System.Drawing.Size(207, 40);
             this.fillBtn.TabIndex = 1;
             this.fillBtn.Text = "填充";
             this.fillBtn.UseVisualStyleBackColor = true;
@@ -140,12 +137,11 @@
             // 
             // MachineTranslate
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MachineTranslate";
-            this.Size = new System.Drawing.Size(281, 246);
+            this.Size = new System.Drawing.Size(422, 369);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
